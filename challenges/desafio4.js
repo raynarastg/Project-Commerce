4151dcfdd0771928fffab9983291d180acfae4ba
Raynara Santiago <raysantiagobz@gmail.com>
2022-12-15 18:58:53 -0300

    req 04

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.produtos.find({
+  $and: [
+    {
+      vendidos: {
+        $gt: 50,
+        $lt: 100,
+      }
+    }
+  ]
+},
+  {
+    _id: 0,
+    vendidos: 1,
+    nome: 1,
+  }).sort({ vendidos: 1 });
